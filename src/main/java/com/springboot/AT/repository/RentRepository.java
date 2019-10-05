dbc24d6d0910093c2de270aaea8662b57ebbd0e3
Adrian <adrian91hg@gmail.com>
2019-10-05 11:40:59 +0200

    RentRepository con carProfit

--- src/main/java/com/springboot/AT/repository/RentRepository.java
+++ src/main/java/com/springboot/AT/repository/RentRepository.java
@@ -1,9 +1,16 @@
 package com.springboot.AT.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDate;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.springboot.AT.dto.ResultRent;
 import com.springboot.AT.entity.Rent;
 
 public interface RentRepository extends JpaRepository<Rent, Integer> {
-
+	
+	@Query(value = "SELECT SUM(price) FROM Rent r WHERE r.car = :idCar AND r.finalRent between ':ini' AND ':fin'")
+	public Rent carProfit(@Param(value = "idCar") Integer idCar, @Param(value="ini") LocalDate fechaIn,@Param(value="fin") LocalDate fechaFin);
 }
